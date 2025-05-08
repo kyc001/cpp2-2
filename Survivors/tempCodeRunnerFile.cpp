a60e56997e9e40d8b7940fda35edce4a993945d9
keyunchao <15551774+kyc001@user.noreply.gitee.com>
2025-05-08 20:23:15 +0800

    1

---
+++ Survivors/tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+
+    char s1[] = "this is a Book", s2[] = "this is a Haik";
+    int i = 0;
+    while(s1[i] = s2[i], i ++);
+    printf("%s\n%s", s1, s2);
+
+    return 0;
+}
