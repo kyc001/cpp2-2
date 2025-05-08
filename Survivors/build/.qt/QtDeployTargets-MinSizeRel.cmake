fd36e830d76e156faf9980afbd4b3e67a2fd274a
keyunchao <15551774+kyc001@user.noreply.gitee.com>
2025-05-08 20:52:49 +0800

    1

---
+++ Survivors/build/.qt/QtDeployTargets-MinSizeRel.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Survivors_FILE D:/Survivors/Survivors/build/MinSizeRel/Survivors.exe)
+set(__QT_DEPLOY_TARGET_Survivors_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Survivors_RUNTIME_DLLS D:/Qt20250127/6.8.1/mingw_64/bin/Qt6MultimediaWidgets.dll;D:/Qt20250127/6.8.1/mingw_64/bin/Qt6Widgets.dll;D:/Qt20250127/6.8.1/mingw_64/bin/Qt6Multimedia.dll;D:/Qt20250127/6.8.1/mingw_64/bin/Qt6Gui.dll;D:/Qt20250127/6.8.1/mingw_64/bin/Qt6Network.dll;D:/Qt20250127/6.8.1/mingw_64/bin/Qt6Core.dll)
